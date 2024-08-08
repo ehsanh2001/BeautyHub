184a270b1a48a79025755a396243e57791dddf2c
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-08-08 13:28:46 -0400

    updated user model and auth files

--- server/models/User.js
+++ server/models/User.js
@@ -1,31 +1,60 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
+// const userSchema = new Schema({
+//   username: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//     trim: true,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//     match: [/.+@.+\..+/, 'Must match an email address!'],
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//     minlength: 5,
+//   },
+//   thoughts: [
+//     {
+//       type: Schema.Types.ObjectId,
+//       ref: 'Thought',
+//     },
+//   ],
+// });
+
 const userSchema = new Schema({
-  username: {
+  firstName: {
     type: String,
     required: true,
     unique: true,
-    trim: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
   },
   email: {
     type: String,
     required: true,
     unique: true,
-    match: [/.+@.+\..+/, 'Must match an email address!'],
+    match: [/.+\@.+\..+/, 'Please fill a valid email address'],
   },
   password: {
     type: String,
     required: true,
-    minlength: 5,
   },
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Thought',
-    },
-  ],
-});
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+})
 
 userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
