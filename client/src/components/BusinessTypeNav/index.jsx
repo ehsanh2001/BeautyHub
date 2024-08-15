fa471823847d1edc12995cfe70328ee36a1a3094
Ehsan Hosseini <ehsanh2001@gmail.com>
2024-08-15 14:50:32 -0400

    Fix Link to business type

--- client/src/components/BusinessTypeNav/index.jsx
+++ client/src/components/BusinessTypeNav/index.jsx
@@ -9,14 +9,15 @@ const BusinessTypeNav = () => {
     <nav className="services-navbar">
       <ul className="nav-list">
         {types.map((type) => {
-          const servicePath = `/search/${type}`;
+          const servicePath = `/`;
           const isActive = location.pathname === servicePath;
 
           return (
             <li className="nav-item" key={type}>
               <Link
-                className={`nav-link ${isActive ? "active" : ""}`}
+                className={`nav-link ${isActive ? "active" : ""} text-light`}
                 to={servicePath}
+                state={{ type: type }}
               >
                 {type}
               </Link>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import HomeHeader from "../components/HomeHeader";
 import BusinessList from "../components/BusinessList";
+import { useLocation } from "react-router-dom";
 
 const Home = () => {
+  const { state } = useLocation();
+  const type = state?.type || "all";
+
   return (
     <main className="container-fluid">
       <div className="flex-row justify-center">
         <HomeHeader />
-        <BusinessList businessType="all" />
+        <BusinessList businessType={type} />
       </div>
     </main>
   );
